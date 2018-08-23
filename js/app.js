cf3b52af96c9e07e556e5e48c3dc1c37db0665bf
Joe Szaf <joeszaf@gmail.com>
2018-08-23 16:00:28 -0400

    start moving functionality of app to MVO structure

--- js/app.js
+++ js/app.js
@@ -3,10 +3,47 @@ $(function(){
   // where the data is contained
   var model = {
 
+    init: function() {
+      // tracks number of cats created, used to create unique IDs for cats
+      var numOfCats = 0;
+
+      // Holds cat objects
+      const cats = [];
+
+      var cat1 = new Cat('Leonardo');
+      var cat2 = new Cat('Lilypad');
+      var cat3 = new Cat('Tallow and Tuba');
+      var cat4 = new Cat('Pico de Gato');
+      var cat5 = new Cat('Ferdinand');
+
+
+    },
+
+    Cat: function(name){
+      this.name = name;
+      numOfCats ++; // increments nubmer of cats as a counter id
+      this.catNum = numOfCats
+      this.clicks = 0; // tracks number of times cat has been clicked
+      this.addCat = function() {
+        catSelector.append(`<option value="cat${this.catNum}"">${this.name}</option>`)
+      };
+
+      this.increment = function() {
+        this.clicks ++;
+        $(`#cat-${this.catNum}`).text(this.clicks);
+      }
+    },
+
   };
 
   // works between the model and the two views
   var octopus = {
+
+    // increments the number of times the cat picture has been clicked, stored in the cat object
+    increment: function(cat){
+      window[cat].clicks ++;
+      $(`#cat-${window[cat].catNum}`).text(window[cat].clicks);
+    },
 
   };
 
@@ -18,62 +55,42 @@ $(function(){
   // the view that displays the selected cat, name, and number of clicks
   var catDisplayView = {
 
+    init: function() {
+      var cat = $('#cat-pic');
+      var clickCount = $('#click-count');
+      var catContainer = $('#cat-container');
+      var catSelector = $('#cat-selector');
+      var catPic = $('.cat-pic');
+    },
+
+    // update cat and information for cat
+    render: function(){
+      catContainer.html(`<div class='cat-picture-div'>
+        <img src='img/cat${this.catNum}.jpg' class='cat-pic' id='cat${this.catNum}'>
+      </div>
+      <div class='cat-details-div'>
+        <h2>${this.name}</h2>
+        <h3>Has been clicked <span id='cat-${this.catNum}'>${this.clicks}</span> times!</h3>
+      </div>`);
+
+      $('img').click(function(e){
+          e.preventDefault();
+          window[this.id].increment();
+      });
+
+    }
+
   };
 
 });
 
 
 
-
-
-var cat = $('#cat-pic');
-var clickCount = $('#click-count');
-var catContainer = $('#cat-container');
-var catSelector = $('#cat-selector');
-var catPic = $('.cat-pic');
-
-// Globals
-var numOfCats = 0;
-
 // Cat object that stores information about the cats to be clicked
 
-function Cat(name){
-  this.name = name;
-  numOfCats ++; // increments nubmer of cats as a counter id
-  this.catNum = numOfCats
-  this.clicks = 0; // tracks number of times cat has been clicked
-  this.addCat = function() {
-    catSelector.append(`<option value="cat${this.catNum}"">${this.name}</option>`)
-  };
 
-  this.increment = function() {
-    this.clicks ++;
-    $(`#cat-${this.catNum}`).text(this.clicks);
-  }
 
-  // update cat and information for cat
-  this.updateCat = function(){
-    catContainer.html(`<div class='cat-picture-div'>
-      <img src='img/cat${this.catNum}.jpg' class='cat-pic' id='cat${this.catNum}'>
-    </div>
-    <div class='cat-details-div'>
-      <h2>${this.name}</h2>
-      <h3>Has been clicked <span id='cat-${this.catNum}'>${this.clicks}</span> times!</h3>
-    </div>`);
 
-    $('img').click(function(e){
-        e.preventDefault();
-        window[this.id].increment();
-    });
-
-  }
-}
-
-var cat1 = new Cat('Leonardo');
-var cat2 = new Cat('Lilypad');
-var cat3 = new Cat('Tallow and Tuba');
-var cat4 = new Cat('Pico de Gato');
-var cat5 = new Cat('Ferdinand');
 
 cat1.addCat();
 cat2.addCat();
@@ -81,8 +98,7 @@ cat3.addCat();
 cat4.addCat();
 cat5.addCat();
 
-// Holds cat objects
-const cats = [];
+
 
 // on selection of cat, updates ui to show selected cats
 catSelector.change(function(e){
